a05fb97d024b3f3540a80cb1d135953df8bdc5ea
jklomp <jan-willem.klomp@visma.com>
2023-06-11 11:42:58 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as sass from 'sass'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
+});
